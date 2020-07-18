f6d85417bcecfbade28918ba24c7912aa3c64166
maruby <49580815+maruby@users.noreply.github.com>
2020-07-18 18:17:10 +0800

    Public Folder 07182020
    added public folder wiht the root html

--- server/src/routes/music.js
+++ server/src/routes/music.js
@@ -6,13 +6,3 @@ router.get('/searchMusic', function(req, res, next) {
     res.send('You are gay');
 });
 
-module.exports = router;
-const Chuchu = () => {
-    router.get('/searchMusic2', function(req, res, next) {
-        res.send('You are gay 2');
-    });
-}
-
-module.export = {
-    Chuchu();
-}
