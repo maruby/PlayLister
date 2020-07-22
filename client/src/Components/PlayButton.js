bb6ded71c00a0a022d76faec850da7d0efc86f7b
maruby <49580815+maruby@users.noreply.github.com>
2020-07-22 15:07:26 +0800

    Update PlayButton.js

--- client/src/Components/PlayButton.js
+++ client/src/Components/PlayButton.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Fab} from '@material-ui/core';
-import { PlayArrowRoundedIcon } from '@material-ui/icons/PlayArrowRounded';
+import PlayArrowRoundedIcon from '@material-ui/icons/PlayArrowRounded';
 
 import './PlayButton.css'
 
