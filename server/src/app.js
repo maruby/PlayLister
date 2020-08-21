70ed0dd56d5128838e16d13a7296daa68ea528d0
maruby <49580815+maruby@users.noreply.github.com>
2020-08-21 21:54:56 +0800

    Configuring  scripts for deployment
    Added scripts on package.json in the root and package.json in the server folder

--- server/src/app.js
+++ server/src/app.js
@@ -8,21 +8,16 @@ var common = require('./common');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+//app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Setup routes
 common.setupRoutes(app);
@@ -31,6 +26,21 @@ common.setupRoutes(app);
 app.use(function(req, res, next) {
   next(createError(404));
 });
+//Static file declaration
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+//production mode
+if(process.env.NODE_ENV === 'production') {  
+  app.use(express.static(path.join(__dirname, 'client/build')));    
+  app.get('*', (req, res) => {
+    res.sendfile(path.join(__dirname, 'client/build/index.html'));
+  })
+}
+
+//build mode
+app.get('*', (req, res) => {  
+  res.sendFile(path.join(__dirname+'/client/public/index.html'));
+})
 
 // error handler
 app.use(function(err, req, res, next) {
